eaa0980f2170a52d0a8769425325604b8e56aa93
Brett Saxon <brettcsaxon@gmail.com>
2020-06-09 20:11:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const minimumSwaps = (arr)=>{
+  let count = 0;
+    for(let i=0; i < arr.length; ++i){
+        while(arr[i] != i+1) {
+            let temp = arr[i];
+            arr[i] = arr[temp-1];
+            arr[temp-1] = temp;  
+            count ++;
+        }
+    }
+    return count;
+}
+
+let arr =  [2,1,3];
+minimumSwaps(arr)
